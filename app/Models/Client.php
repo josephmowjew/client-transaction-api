46e6198cdb3cb55d0e23a16900472405eb5a0a82
Joseph-Mojoo <61784861+Joseph-Mojoo@users.noreply.github.com>
2025-03-17 21:28:03 +0200

    Update Client model to include 'surname' in fillable properties and replace 'address' with 'income'. Import HasMany relation for transaction management.

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Client extends Model
 {
@@ -11,9 +12,10 @@ class Client extends Model
 
     protected $fillable = [
         'name',
+        'surname',
         'email',
         'phone',
-        'address'
+        'income'
     ];
 
     public function transactions(): HasMany
