631e6382a20e7ac7fe4f775b7f5c4a7282f3300d
Joseph-Mojoo <61784861+Joseph-Mojoo@users.noreply.github.com>
2025-03-17 21:04:30 +0200

    # Mode:

---
+++ app/Http/Resources/TransactionResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TransactionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'client_id' => $this->client_id,
+            'amount' => $this->amount,
+            'type' => $this->type,
+            'description' => $this->description,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
