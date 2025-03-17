090913e99bc3aef7fc8ceea4712033b0640b6ba9
Joseph-Mojoo <61784861+Joseph-Mojoo@users.noreply.github.com>
2025-03-17 21:06:21 +0200

    Add ClientRequest and TransactionRequest classes for handling validation rules in client and transaction requests.

---
+++ app/Http/Requests/ClientRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ClientRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules()
+    {
+        $rules = [
+            'name' => 'required|string|max:255',
+            'surname' => 'required|string|max:255',
+            'email' => 'required|email|unique:clients,email',
+            'phone' => 'nullable|string|max:20',
+            'income' => 'required|numeric|min:0',
+        ];
+
+        if ($this->method() === 'PUT') {
+            $rules['email'] = 'required|email|unique:clients,email,' . $this->route('client');
+        }
+
+        return $rules;
+    }
+}
